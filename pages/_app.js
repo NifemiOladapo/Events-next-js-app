e519c86d4f5efc1d5e1fd5afedca459a5011e733
kollydap <dapkolly@gmail.com>
2023-07-17 16:56:33 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-import MainHeader from "@/components/MainHeader";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <MainHeader />
       <Component {...pageProps} />
     </>
   );
